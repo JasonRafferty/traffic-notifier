2ea2cc8f3f4d65707b8f3a23a1251458cc01f73d
Jason Rafferty <jasonraff@hotmail.co.uk>
2026-06-29 22:16:38 +0100

    feat: add homeGrounds team-to-venue-id map

---
+++ src/js/data/homeGrounds.js
@@ -0,0 +1,54 @@
+export const homeGrounds = {
+  // Manchester
+  "Manchester United":        "KovZ9177z3V",
+  "Manchester City":          "KovZ9177YTX",
+
+  // London
+  "Arsenal":                  "KovZ9177z6L",
+  "Chelsea":                  "KovZ9177z1y",
+  "Tottenham Hotspur":        "KovZ9177z3C",
+  "West Ham United":          "KovZ9177z6g",
+  "Crystal Palace":           "KovZ9177z5X",
+  "Fulham":                   "KovZ9177z4X",
+  "Queens Park Rangers":      "KovZ9177z8p",
+
+  // Birmingham
+  "Aston Villa":              "KovZ9177z9c",
+  "Birmingham City":          "KovZ9177z9B",
+
+  // Liverpool
+  "Liverpool":                "KovZ9177z6t",
+  "Everton":                  "KovZ9177z6u",
+
+  // Newcastle
+  "Newcastle United":         "KovZ9177z8n",
+
+  // Sunderland
+  "Sunderland":               "KovZ9177z8y",
+
+  // Leeds
+  "Leeds United":             "KovZ9177z9x",
+
+  // Sheffield
+  "Sheffield United":         "KovZ9177z4d",
+  "Sheffield Wednesday":      "KovZ9177z4s",
+
+  // Nottingham
+  "Nottingham Forest":        "KovZ9177z3b",
+
+  // Leicester
+  "Leicester City":           "KovZ9177z2f",
+
+  // Southampton
+  "Southampton":              "KovZ9177z2h",
+
+  // Wolverhampton
+  "Wolverhampton Wanderers":  "KovZ9177z2j",
+
+  // Glasgow
+  "Celtic":                   "KovZ9177z9r",
+  "Rangers":                  "KovZ9177z9t",
+
+  // Cardiff
+  "Cardiff City":             "KovZ9177z9A",
+};
